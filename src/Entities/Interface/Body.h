4443c91e9fd7f39f79b96cec5c13e0ca809aa148
Mateus Back <m.back@nextage.com.br>
2025-03-19 21:37:44 -0300

    refactor: enhance Body class constructors for improved flexibility

--- src/Entities/Interface/Body.h
+++ src/Entities/Interface/Body.h
@@ -12,11 +12,30 @@ protected:
     public:
     	Body(float x = 0, float y = 0, float w = 0, float h = 0, bool collision = false, bool visible = true)
         : has_collision(collision), is_visible(visible) {
-        rect.x = x;
-        rect.y = y;
-        rect.w = w;
-        rect.h = h;
-    }
+            this->rect.x = x;
+            this->rect.y = y;
+            this->rect.w = w;
+            this->rect.h = h;
+            this->has_collision = collision;
+            this->is_visible = visible;
+        }
+
+        Body(SDL_FRect rect, bool collision = false, bool visible = true)
+        : has_collision(collision), is_visible(visible) {
+            this->rect = rect;
+            this->has_collision = collision;
+            this->is_visible = visible;
+        }
+
+        Body(SDL_FPoint position, SDL_FPoint size, bool collision = false, bool visible = true)
+        : has_collision(collision), is_visible(visible) {
+            this->rect.x = position.x;
+            this->rect.y = position.y;
+            this->rect.w = size.x;
+            this->rect.h = size.y;
+            this->has_collision = collision;
+            this->is_visible = visible;
+        }
 };
 
 #endif

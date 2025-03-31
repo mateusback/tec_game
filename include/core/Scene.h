1e52a131f11708f42a8ab80eb0001c17022f1006
Mateus Back <m.back@nextage.com.br>
2025-03-31 12:34:35 -0300

    feat: add TODO.md for team control

--- include/core/Scene.h
+++ include/core/Scene.h
@@ -7,10 +7,10 @@ namespace Core
     class Scene 
     {
     public:
-    virtual ~Scene() = default;
-    virtual void handleEvent(const SDL_Event& event) = 0;
-    virtual void update(float deltaTime) = 0;
-    virtual void render(SDL_Renderer* renderer) = 0;
+        virtual ~Scene() = default;
+        virtual void handleEvent(const SDL_Event& event) = 0;
+        virtual void update(float deltaTime) = 0;
+        virtual void render(SDL_Renderer* renderer) = 0;
     };
     
 }
